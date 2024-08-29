e5479b7417c1393b70f50241407373d127489243
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-08-28 23:27:10 -0300

    Primeira aula de C

---
+++ aula1/teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    float n1, n2, soma;
+    n1 = 5;
+    n2 = 23;
+    soma = n1 + n2;
+    printf("The result of sum is: %.0f", soma);
+}
